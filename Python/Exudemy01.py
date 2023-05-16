43943d895470e9b1f0596ef70e7dc955e7adc2c7
Leandro Campagnaro <57913956+lcampagnaro@users.noreply.github.com>
2023-05-16 09:58:52 -0300

    cursopythonfinalizado first commit

---
+++ Python/Exudemy01.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x= [1, 2, 3, 4, 5]
+y= [2, 5, 7, 3, 4]
+
+plt.title('Teste primeiro gráfico')
+plt.xlabel('Eixo X - Teste')
+plt.ylabel('Eixo Y - Teste')
+plt.bar(x, y)
+plt.show()
